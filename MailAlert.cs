975e9f4444bffa86d3f9558b750330b26053a582
Praveenkumar210995 <pravin21995@gmail.com>
2024-11-02 18:17:33 +0530

    Update MailAlert.cs

--- MailAlert.cs
+++ MailAlert.cs
@@ -1,17 +1,20 @@
 using System;
+using System.Collections.Generic;
+
 namespace BatteryTemperature
 {
     class MailAlert
     {
+        private static readonly Dictionary<BreachType, string> BreachMessages = new()
+        {
+            { BreachType.TooLow, "Hi, the temperature is too low." },
+            { BreachType.TooHigh, "Hi, the temperature is too high." }
+        };
+
         public void SendToEmail(BreachType breachType)
         {
             string recipient = "a.b@c.com";
-            string message = string.Empty;
-
-            if (breachType == BreachType.TooLow)
-                message = "Hi, the temperature is too low.";
-            else if (breachType == BreachType.TooHigh)
-                message = "Hi, the temperature is too high.";
+            string message = BreachMessages.GetValueOrDefault(breachType, string.Empty);
 
             if (!string.IsNullOrEmpty(message))
             {
