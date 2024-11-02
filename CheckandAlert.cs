e422181ad995778ed96d7cb7cf366bbd1fd29dcd
Praveenkumar210995 <pravin21995@gmail.com>
2024-11-02 17:20:00 +0530

    Update CheckandAlert.cs

--- CheckandAlert.cs
+++ CheckandAlert.cs
@@ -6,6 +6,18 @@ namespace BatteryTemperature
         ControllerAlert controllerAlert = new ControllerAlert();
         TypewiseAlert typewiseAlerts = new TypewiseAlert();
         MailAlert mailAlert = new MailAlert();
+
+        private readonly ControllerAlert _controllerAlert;
+        private readonly TypewiseAlert _typewiseAlerts;
+        private readonly MailAlert _mailAlert;
+
+        public CheckandAlert(ControllerAlert controllerAlert, TypewiseAlert typewiseAlerts, MailAlert mailAlert)
+        {
+            _controllerAlert = controllerAlert;
+            _typewiseAlerts = typewiseAlerts;
+            _mailAlert = mailAlert;
+        }
+
         public void CheckAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC)
         {
             BreachType breachType = typewiseAlerts.ClassifyTemperatureBreach(batteryChar.CoolingType, temperatureInC);
