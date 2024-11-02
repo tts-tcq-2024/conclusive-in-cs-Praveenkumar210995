ecc36b05715b1c90f6fb6c88c3f735d9c191e317
Praveenkumar210995 <pravin21995@gmail.com>
2024-11-02 18:15:34 +0530

    Update TypeWiseAlert.cs

--- TypeWiseAlert.cs
+++ TypeWiseAlert.cs
@@ -1,35 +1,32 @@
 using System;
+using System.Collections.Generic;
+
 namespace BatteryTemperature
 {
-        public class TypewiseAlert
+    public class TypewiseAlert
+    {
+        private static readonly Dictionary<CoolingType, double> CoolingLimits = new()
         {
+            { CoolingType.PassiveCooling, 35 },
+            { CoolingType.MediumActiveCooling, 40 },
+            { CoolingType.HighActiveCooling, 45 }
+        };
 
-            public static BreachType InferBreach(double value, double lowerLimit, double upperLimit)
+        public BreachType ClassifyTemperatureBreach(CoolingType coolingType, double temperatureInC)
+        {
+            if (!CoolingLimits.TryGetValue(coolingType, out double upperLimit))
             {
-                return value <= lowerLimit ? BreachType.TooLow :
-                       value >= upperLimit ? BreachType.TooHigh :
-                       BreachType.Normal;
+                throw new ArgumentOutOfRangeException(nameof(coolingType), "Invalid cooling type");
             }
 
-            private const double PassiveCoolingLimit = 35;
-            private const double HighActiveCoolingLimit = 45;
-            private const double MediumActiveCoolingLimit = 40;
-
-            public BreachType ClassifyTemperatureBreach(CoolingType coolingType, double temperatureInC)
-            {
-                double upperLimit;
-
-                if (coolingType == CoolingType.PassiveCooling)
-                    upperLimit = PassiveCoolingLimit;
-                else if (coolingType == CoolingType.HighActiveCooling)
-                    upperLimit = HighActiveCoolingLimit;
-                else if (coolingType == CoolingType.MediumActiveCooling)
-                    upperLimit = MediumActiveCoolingLimit;
-                else
-                    throw new ArgumentOutOfRangeException(nameof(coolingType), "Invalid cooling type");
-
-                return InferBreach(temperatureInC, 0, upperLimit);
-            }
+            return InferBreach(temperatureInC, 0, upperLimit);
         }
 
+        public static BreachType InferBreach(double value, double lowerLimit, double upperLimit)
+        {
+            return value <= lowerLimit ? BreachType.TooLow :
+                   value >= upperLimit ? BreachType.TooHigh :
+                   BreachType.Normal;
+        }
+    }
 }
