d9f0f82f1ebc1e315a22cbf4d9725b954c38a916
Praveenkumar210995 <pravin21995@gmail.com>
2024-11-02 17:19:22 +0530

    Update CooloingType.cs

--- CooloingType.cs
+++ CooloingType.cs
@@ -1,9 +1,10 @@
+using System;
+
 namespace BatteryTemperature
 {
     public class BatteryCharacter
     {
         public CoolingType CoolingType { get; set; }
-        CheckandAlert checkandAlert = new CheckandAlert();
     }
 
 }
